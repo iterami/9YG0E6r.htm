d518720fb488d22e17f442c4718f72b317cb7e9c
honzi <czechinthemail@gmail.com>
2017-07-23 12:57:55 -0700

    moved randomize() into data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,47 @@
 'use strict';
 
 function load_data(){
-    canvas_buffer.strokeStyle = core_storage_data['color-positive'];
     randomize();
+}
+
+function randomize(){
+    core_entity_remove_all();
+
+    canvas_buffer.strokeStyle = core_storage_data['color-positive'];
+
+    var loop_counter = core_storage_data['vertices'] - 1;
+    var first_id = false;
+    var parent_id = '';
+    do{
+        var this_id = core_uid();
+        if(!first_id){
+            first_id = this_id;
+        }
+
+        core_entity_create({
+          'id': this_id,
+          'properties': {
+            'dx': 0,
+            'dy': 0,
+            'loop': core_random_integer({
+              'max': 360,
+            }),
+            'parent': parent_id,
+            'radius': Math.random() * properties['radius'] + 5,
+            'speed': core_random_integer({
+              'max': properties['speed'],
+            }) - properties['speed'] / 2,
+            'x': core_random_integer({
+              'max': properties['x'],
+            }) - properties['x'] / 2,
+            'y': core_random_integer({
+              'max': properties['y'],
+            }) - properties['y'] / 2,
+          },
+        });
+
+        parent_id = this_id;
+    }while(loop_counter--);
+
+    core_entities[first_id]['parent'] = parent_id;
 }
